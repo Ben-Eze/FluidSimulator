971cffde856dc0709b2a68af5a9a324cb1b08365
Ben-Eze <benedicteze@gmail.com>
2024-09-20 09:50:32 +0100

    Triangular mesh: add description to reference_tri.py

--- theory/reference_tri.py
+++ theory/reference_tri.py
@@ -1,3 +1,36 @@
+################################################################################
+##
+##  File: reference_tri.py
+##
+##  The MIT License
+##
+##  Copyright (c) 2006 Division of Applied Mathematics, Brown University (USA),
+##  Department of Aeronautics, Imperial College London (UK), and Scientific
+##  Computing and Imaging Institute, University of Utah (USA).
+##
+##  Permission is hereby granted, free of charge, to any person obtaining a
+##  copy of this software and associated documentation files (the "Software"),
+##  to deal in the Software without restriction, including without limitation
+##  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+##  and/or sell copies of the Software, and to permit persons to whom the
+##  Software is furnished to do so, subject to the following conditions:
+##
+##  The above copyright notice and this permission notice shall be included
+##  in all copies or substantial portions of the Software.
+##
+##  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+##  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+##  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+##  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+##  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+##  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+##  DEALINGS IN THE SOFTWARE.
+##
+##  Description:  Calculate the time derivative of the scalar function X on a ##                reference triangular domain.
+##                Since a single element is used with linear trial functions, ##                the second derivative (and thus diffusion) should be zero!
+##
+################################################################################
+
 import numpy as np
 
 def get_sigma_ij(ai, bi, gi, aj, bj, gj):
